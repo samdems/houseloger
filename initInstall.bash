dbd17e8c545dfef1ee351aa5d5fc8af60047de70
Sam Cross <git@samcross.uk>
2023-10-20 15:21:49 +0000

    fixed docker

--- initInstall.bash
+++ initInstall.bash
@@ -1,8 +1,6 @@
 docker run --rm \
     -u "$(id -u):$(id -g)" \
-    -v $(pwd):/opt \
-    -w /opt \
-    laravelsail/php80-composer:latest \
+    -v "$(pwd):/var/www/html" \
+    -w /var/www/html \
+    laravelsail/php82-composer:latest \
     composer install --ignore-platform-reqs
-
-./vendor/bin/sail npm install
