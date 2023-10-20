905ee003f921c9b8eb9af46498d08b2fb0df9f69
sam cross <sam.cross@gmx.com>
2023-10-20 11:34:41 +0100

    3 set up GitHub pipeline and code checking (#7)
    * added workflow for testing
    
    * moved workflow
    
    * fixed name
    
    * added staticAnalysis and env.ci
    
    * update
    
    * test
    
    * test
    
    * update tests
    
    * update test
    
    * update
    
    * added node
    
    * added stan
    
    * moved node to first
    
    * fixed file error
    
    * fixed node
    
    * fixed install added node build
    
    * test
    
    * update .env
    
    * update
    
    * test
    
    * added phpstan
    
    * phpstan
    
    * added codesniffer
    
    * fixed name
    
    * added pint and set to psr12
    
    * fixed pint
    
    * fixed for phpcs and pint
    
    ---------
    
    Co-authored-by: Sam Cross <git@samcross.uk>

---
+++ initInstall.bash
@@ -0,0 +1,8 @@
+docker run --rm \
+    -u "$(id -u):$(id -g)" \
+    -v $(pwd):/opt \
+    -w /opt \
+    laravelsail/php80-composer:latest \
+    composer install --ignore-platform-reqs
+
+./vendor/bin/sail npm install
